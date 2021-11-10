0aa94736adc0fb00e4702511560919cff3a7f994
doguskukul <doguskukul@gmail.com>
2021-11-10 20:21:23 +0300

    navbar eklendi, routes ayarlandı

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { AppRoutes } from  './app.routing';
 import { AppComponent } from './app.component';
-import { MainPageComponent } from './main-page/main-page.component';
+import { HomeComponent } from './home/home.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { ProductsComponent } from './products/products.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import {RouterModule} from "@angular/router";
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainPageComponent
+    HomeComponent,
+    AboutUsComponent,
+    ProductsComponent,
+    ContactUsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(AppRoutes, { useHash: true })
   ],
   providers: [],
   bootstrap: [AppComponent]
